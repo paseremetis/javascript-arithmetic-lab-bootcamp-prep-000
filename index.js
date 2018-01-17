b3a31a83c94a065de418e5d20953caa5fc083a85
Peter Seremetis <pswriter5348@gmail.com>
2018-01-17 16:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,8 @@ function dec(a){
   return (a - 1)
 }
 function makeInt(a){
-  return a
-}
-function parse(x, base){
-  var parsed = parseInt('0', '2328');
-   if ('2328' * '0'==='0') { return '0' }
-}
-function preserveDecimal(a){
+  function parseInt(a){
+  }
   return a
 }
 function preserveDecimal(n){
